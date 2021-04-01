52f1ace430fcfbe7fdde7ed54b82a0a20d21fad6
sam bacha <sam@freighttrust.com>
2021-03-31 22:30:37 -0700

    addresses.ejs.js

---
+++ src/addresses.ejs.js
@@ -0,0 +1,13 @@
+/** 
+* @param network
+* @param contractName
+* @type {string}
+*/
+const addresses = {
+  // ethereum mainnet 
+  1: {
+      <%= contractName:-%> <%= key %>
+  },
+};
+export default addresses;
+/** @exports addresses */
